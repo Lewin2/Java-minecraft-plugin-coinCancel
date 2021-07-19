d13e62ab9aa8c09522c900f89295fb471749e171
Woojin Lim <62597736+Lewin2@users.noreply.github.com>
2021-07-19 12:05:11 +0900

    Add files via upload

---
+++ src/main/java/me/lewin/dellunacoin/CancelEvent.java
@@ -0,0 +1,56 @@
+package me.lewin.dellunacoin;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class CancelEvent implements Listener {
+    @EventHandler
+    private void onPlayerUse(PlayerInteractEvent event){
+        Player player = event.getPlayer();
+        ItemStack item = player.getItemInHand();
+        if (isGold(item)){
+            event.setCancelled(true);
+        }
+        else if (isSilver(item)){
+            event.setCancelled(true);
+        }
+        else if (isCopper(item)){
+            event.setCancelled(true);
+        }
+    }
+
+    private boolean isGold(ItemStack item){
+        if (item.getType() == Material.RED_DYE){
+            if (item.getItemMeta().hasCustomModelData()){
+                if (item.getItemMeta().getCustomModelData() == 1003){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    private boolean isSilver(ItemStack item){
+        if (item.getType() == Material.BLUE_DYE){
+            if (item.getItemMeta().hasCustomModelData()){
+                if (item.getItemMeta().getCustomModelData() == 1003){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    private boolean isCopper(ItemStack item){
+        if (item.getType() == Material.GREEN_DYE){
+            if (item.getItemMeta().hasCustomModelData()){
+                if (item.getItemMeta().getCustomModelData() == 1003){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
---
+++ src/main/java/me/lewin/dellunacoin/Main.java
@@ -0,0 +1,12 @@
+package me.lewin.dellunacoin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(new CancelEvent(), this);
+    }
+}
